60b53e8ee60b93e2c0a0cb1c571b2e60f94ac253
etothepowerof26 <jdhale2004@gmail.com>
2020-01-18 22:54:24 -0500

    Add in the bait sale fix from Metastruct/fishingmod

--- lua/fishing_mod/sv_networking.lua
+++ lua/fishing_mod/sv_networking.lua
@@ -63,6 +63,8 @@ end
 function fishingmod.SetBaitSale(bait, multiplier, ply)
 	fishingmod.BaitTable[bait].multiplier = multiplier
 	
+	local hasplayers = player.GetHumans()[1]
+	if not IsValid(ply) and not hasplayers then return end
 	
 	net.Start("Fishingmod:BaitPrices")
 	net.WriteString(bait)
@@ -70,7 +72,7 @@ function fishingmod.SetBaitSale(bait, multiplier, ply)
 	
 	if IsValid(ply) then
 		net.Send(ply)
-	else
+	elseif hasplayers then
 		net.Broadcast()
 	end
 end
